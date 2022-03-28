9c190fc5e6ef3f4a2cd7a9041b7dd395e50acb26
= <niccolosottile@outlook.com>
2022-03-28 12:08:56 +0100

    updated question 1

--- question_1/VigenereCipher.java
+++ question_1/VigenereCipher.java
@@ -13,4 +13,14 @@ public class VigenereCipher implements Cipher {
         return "";
     }
 
+    public static void main(String[] args) {
+    
+        VigenereCipher trial_cipher = new VigenereCipher();
+
+        System.out.println(trial_cipher.encrypt("encrypt_check.txt", "key_check.txt"));
+
+        System.out.println(trial_cipher.decrypt("decrypt_check.txt", "key_check.txt"));
+
+    }
+
 }
