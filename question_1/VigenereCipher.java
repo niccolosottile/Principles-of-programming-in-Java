315c72bdc6b767d52073d52079efc1e4754326ae
= <niccolosottile@outlook.com>
2022-03-28 12:05:05 +0100

    created structure file for question 1

---
+++ question_1/VigenereCipher.java
@@ -0,0 +1,16 @@
+package question_1;
+
+// Implementing the Vigenère chiper class using the cipher interface
+public class VigenereCipher implements Cipher {
+
+    @Override
+    public String encrypt(String message_filename, String key_filename){
+        return "";
+    }
+
+    @Override
+    public String decrypt(String message_filename, String key_filename){
+        return "";
+    }
+
+}
