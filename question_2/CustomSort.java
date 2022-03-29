98dfa98193c6b26bb5236d2d86d5939514ad92a5
= <niccolosottile@outlook.com>
2022-03-29 11:54:29 +0100

    started question 2

---
+++ question_2/CustomSort.java
@@ -0,0 +1,36 @@
+package question_2;
+
+import java.util.ArrayList;
+
+public class CustomSort implements SortingInterface {
+
+	@Override
+	public void setValues(ArrayList<Double> values){
+
+    }
+
+    // PRIORITY
+	@Override
+	public ArrayList<Integer> getGaps(){
+
+        ArrayList<Integer> gaps = new ArrayList<Integer>();
+
+        return gaps;
+    }
+
+	@Override
+	public void add(Double value){
+
+    }
+
+	@Override
+	public void remove(int index){
+        
+    }
+
+    // PRIORITY
+    @Override
+	public void sort(){
+        
+    }
+}
