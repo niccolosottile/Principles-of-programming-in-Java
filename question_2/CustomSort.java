0c672951654d22531bb38a761a427b0903093236
= <niccolosottile@outlook.com>
2022-04-01 13:26:53 +0100

    finished question 2

--- question_2/CustomSort.java
+++ question_2/CustomSort.java
@@ -14,27 +14,11 @@ public class CustomSort implements SortingInterface {
         this.customSorted = new ArrayList<>(values);
 
         // Sort the values using the sort function
-        sort();
+        this.sort();
     }
 
-    // Method calculates the power of an int
-    private int pow(int n, int p){
-        
-        while(p != 1){
-
-            n = n * n;
-
-            p--;
-        }
-
-        return n;
-    }
-
-    // PRIORITY
 	@Override
 	public ArrayList<Integer> getGaps(){
-
-        // FOLLOWED THE PSEDUCODE
 
         ArrayList<Integer> temp = new ArrayList<Integer>();
         ArrayList<Integer> gaps = new ArrayList<Integer>();
@@ -47,7 +31,7 @@ public class CustomSort implements SortingInterface {
 
             temp.add(gap);
 
-            gap = pow(2, i) - 1;
+            gap = (int) Math.pow(2, i) - 1;
 
             i++;
         }
@@ -61,86 +45,30 @@ public class CustomSort implements SortingInterface {
         return gaps;
     }
 
-    // Method used to move the array list forward by 1 element, starting at index
-    private void moveForward(int index){
-
-        for(int i = index; i < (this.customSorted.size() - 1); i++){
-
-            // If at last element, add a new element 
-            if(i == (this.customSorted.size() - 1)){
-                this.customSorted.add(this.customSorted.get(i));
-                break;
-            }
-
-            // Set next element equal to current element
-            this.customSorted.set(i + 1, this.customSorted.get(i));
-
-        }
-
-    }
-
 	@Override
 	public void add(Double value){
 
-        // Loop through the custom sorted array list
-        for(int i = 0; i < this.customSorted.size(); i++){
+        // Add value to sorted array list
+        this.customSorted.add(value);
 
-            // if i is equal to size - 1, check previous value
-            if(i == (this.customSorted.size() - 1)){
-                if(value >= this.customSorted.get(i)){
-
-                    // Add value at the end
-                    this.customSorted.add(value);
-
-                }
-            }
-
-            // If i is bigger than 0 and smaller than size - 1, check in between
-            if(i > 0 && i < (this.customSorted.size() - 1)){
-                if(value >= this.customSorted.get(i) && value <= this.customSorted.get(i + 1)){
-
-                    // Switch all values 1 position forward from i + 1
-                    moveForward(i + 1);
-
-                    // Set position i + 1 to value
-                    this.customSorted.set(i + 1, value);
-
-                }
-
-            }
-
-            // If i is equal to 0, check next value
-            if(i == 0){
-                if(value <= this.customSorted.get(i)){
-
-                    // Switch all values 1 position forward from i
-                    moveForward(i);
-
-                    // Set current position i to value
-                    this.customSorted.set(i, value);
-
-                }
-            }
-        }
+        // Sort the array list
+        this.sort();
     }
 
 	@Override
 	public void remove(int index){
         
         // If not out of bounds, remove the value at the specified index
-        if(index < this.customSorted.size()){
+        if(index >= 0 && index < this.customSorted.size()){
             this.customSorted.remove(index);
         }
     }
 
-    // PRIORITY
     @Override
 	public void sort(){
 
-        // FOLLOWED THE PSEDUCODE
-
         int n = this.customSorted.size();
-        ArrayList<Integer> gaps = getGaps();
+        ArrayList<Integer> gaps = this.getGaps();
 
         for(int q = 0; q < gaps.size(); q++){
 
@@ -150,7 +78,7 @@ public class CustomSort implements SortingInterface {
 
                 int j = 0;
 
-                for(j = i; j <= gaps.get(q); j -= gaps.get(q)){
+                for(j = i; j >= gaps.get(q); j -= gaps.get(q)){
 
                     if(this.customSorted.get(j - gaps.get(q)) <= temp){
                         break;
@@ -188,6 +116,14 @@ public class CustomSort implements SortingInterface {
         values.add(5.1);
 
         gapSort.setValues(values);
+
+        gapSort.printAll();
+
+        gapSort.remove(3);
+
+        gapSort.printAll();
+
+        gapSort.add(2.345);
 
         gapSort.printAll();
 
